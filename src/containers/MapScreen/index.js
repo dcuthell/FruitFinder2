128619e4bd756e1acff58f4e5448c7b819c1733c
dcuthell <dcc1447@gmail.com>
2018-04-03 22:33:40 -0700

    test

--- src/containers/MapScreen/index.js
+++ src/containers/MapScreen/index.js
@@ -10,22 +10,72 @@ import ActionCreators from '../../actions/index'
 import getViewCoords from '../../selectors/viewCoords';
 import styles from './styles'
 
+const INIT_REGION = {
+  latitude: 41.8962667,
+  longitude: 11.3340056,
+  latitudeDelta: 12,
+  longitudeDelta: 12
+}
+
+
 class MapScreen extends React.Component {
+
+  renderCluster = (cluster, onPress) => {
+    const pointCount = cluster.pointCount,
+          coordinate = cluster.coordinate,
+          clusterId = cluster.clusterId
+
+    // use pointCount to calculate cluster size scaling
+    // and apply it to "style" prop below
+
+    // eventually get clustered points by using
+    // underlying SuperCluster instance
+    // Methods ref: https://github.com/mapbox/supercluster
+    const clusteringEngine = this.map.getClusteringEngine(),
+          clusteredPoints = clusteringEngine.getLeaves(clusterId, 100)
+
+    return (
+      <Marker coordinate={coordinate} onPress={onPress}>
+        <View style={styles.myClusterStyle}>
+          <Text style={styles.myClusterTextStyle}>
+            {pointCount}
+          </Text>
+        </View>
+        {
+          /*
+            Eventually use <Callout /> to
+            show clustered point thumbs, i.e.:
+            <Callout>
+              <ScrollView>
+                {
+                  clusteredPoints.map(p => (
+                    <Image source={p.image}>
+                  ))
+                }
+              </ScrollView>
+            </Callout>
+
+            IMPORTANT: be aware that Marker's onPress event isn't really consistent when using Callout.
+           */
+        }
+      </Marker>
+    )
+  }
+
+  renderMarker = (data) => <Marker key={data.id || Math.random()} coordinate={data.location} />
 
   render() {
 
     const {height, width} = Dimensions.get('window');
 
     return (
-      <View style={styles.container}>
-        <MapView
-          style={{height: height, width: width, flex: 1}}
-          showsUserLocation={true}
-          region={this.props.viewCoords}>
-          <Marker coordinate={{longitude: -122.68, latitude: 45.51}}/>
-          <Marker coordinate={{longitude: -122.69, latitude: 45.52}}/>
-        </MapView>
-      </View>
+      <ClusteredMapView
+          style={{flex: 1}}
+          data={this.state.data}
+          initialRegion={INIT_REGION}
+          ref={(r) => { this.map = r }}
+          renderMarker={this.renderMarker}
+          renderCluster={this.renderCluster} />
     );
   }
 }
