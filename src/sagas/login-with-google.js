ed34e59b66e616f3f18addb3074292a1e72e25b5
David Cuthell <dcc1447@gmail.com>
2018-04-07 17:34:20 -0700

    add google authentication for firebase auth

--- src/sagas/login-with-google.js
+++ src/sagas/login-with-google.js
@@ -1,6 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import firebase from 'react-native-firebase';
-import { GoogleSignIn } from 'react-native-google-signin';
+import { GoogleSignin } from 'react-native-google-signin';
 
 import { LOGIN_WITH_GOOGLE } from '../lib/constants/actions';
 import setTreeList from '../actions/set-tree-list';
@@ -10,10 +10,15 @@ import getViewCoords from '../selectors/viewCoords';
 function* loginWithGoogle(navigator) {
   console.log("saga started");
   console.log(navigator);
+
   try {
-    const data = yield GoogleSignIn.signIn();
+    yield GoogleSignin.configure();
+    console.log("1");
+    const data = yield GoogleSignin.signIn();
     const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+    console.log("2");
     const currentUser = yield firebase.auth().signInAndRetrieveDataWithCredential(credential);
+    console.log("3");
     console.info(JSON.stringify(currentUser.user.toJSON()));
   } catch (error){
     console.warn(error);
