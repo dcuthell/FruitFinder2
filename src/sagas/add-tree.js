ca2f51707aea945333250d7e38c19dd7e06bf63b
David Cuthell <dcc1447@gmail.com>
2018-04-09 13:53:12 -0700

    add to firebase for tree

--- src/sagas/add-tree.js
+++ src/sagas/add-tree.js
@@ -8,14 +8,25 @@ import getTreeList from '../selectors/treeList';
 import getViewCoords from '../selectors/viewCoords';
 import addToTreeList from '../actions/add-to-tree-list';
 
-
+function makeFirebaseTree(tree){
+  const id = tree.id;
+  return({
+      "Common" : tree.type,
+      "Edible" : tree.edible,
+      "Condition" : tree.condition,
+      "Size" : tree.size,
+      "X" : tree.location.longitude,
+      "Y" : tree.location.latitude
+  });
+}
 function* addTree(action) {
   const tree = action.payload;
-  console.log( tree );
   yield put(addToTreeList(tree));
-  console.log("nope");
+  yield firebase.database().ref('/log'+`/${tree.id}`).set(makeFirebaseTree(tree));
 }
 
 export default function* watchAddTree() {
   yield takeLatest(ADD_TREE, addTree);
 }
+
+// id: index, location: { longitude: oldTree.X, latitude: oldTree.Y }, type: oldTree.Common, edible: oldTree.Edible, condition: oldTree.Condition, size: oldTree.Size
