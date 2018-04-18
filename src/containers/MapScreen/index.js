b8ac034fd392f1623ccc19a464fe390c41341679
David Cuthell <dcc1447@gmail.com>
2018-04-18 12:06:25 -0700

    cleanup mapscreen

--- src/containers/MapScreen/index.js
+++ src/containers/MapScreen/index.js
@@ -30,127 +30,34 @@ class MapScreen extends React.Component {
 
   constructor(props){
     super(props)
-
-    this.state = {
-      pins: [
-        { id: 0,
-          location: {
-            longitude: -122.68,
-            latitude: 45.51
-          }
-        },
-        { id: 1,
-          location: {
-            longitude: -122.681,
-            latitude: 45.511
-          }
-        },
-        { id: 2,
-          location: {
-            longitude: -122.682,
-            latitude: 45.512
-          },
-          type: "FCUK",
-          edible: "POITSOD",
-          condition: "TERRIBLE",
-          size: "YUGEE"
-        },
-        { id: 3,
-          location: {
-            longitude: -122.679,
-            latitude: 45.509
-          }
-        },
-        { id: 4,
-          location: {
-            longitude: -122.678,
-            latitude: 45.508
-          }
-        },
-        { id: 5,
-          location: {
-            longitude: -122.677,
-            latitude: 45.507
-          }
-        }
-      ]
-    }
-    this.renderMarker = this.renderMarker.bind(this)
-    this.renderCluster = this.renderCluster.bind(this)
-    console.log("fetchfire");
     this.props.fetchTreeList();
   }
 
-  componentDidMount(){
-
-  }
-
-  renderCluster = (cluster, onPress) => {
-    const pointCount = cluster.pointCount,
-          coordinate = cluster.coordinate,
-          clusterId = cluster.clusterId
-
-    return (
-      <Marker identifier={`cluster-${clusterId}`} coordinate={coordinate} onPress={onPress}>
-        <View style={styles.clusterContainer}>
-          <ImageBackground source={require('../../images/tree128.png')} style={styles.clusterImage} >
-            <Text style={styles.clusterText}>
-              {pointCount}
-            </Text>
-          </ImageBackground>
-        </View>
-      </Marker>
-    )
-  }
-
-  renderMarker = (pin) => {
-    // if(pin.id === "300069"){
-    //   console.log("WOOOOO");
-    // }
-    // console.log("render for :" + pin.id);
-    return (
-      <Marker identifier={`pin-${pin.id}`} key={pin.id} coordinate={pin.location} image={require('../../images/tree128.png')}>
-        <Callout>
-          <Text>Tree type: {pin.type}</Text>
-          <Text>Fruit or Nut: {pin.edible}</Text>
-          <Text>Condition: {pin.condition}</Text>
-          <Text>Size: {pin.size}</Text>
-          <Text>Id: {pin.id}</Text>
-        </Callout>
-      </Marker>
-    )
-  }
-
-// {longitudeDelta: 0.0002759322523928631, latitudeDelta: 0.00026995994027601, longitude: -122.67964219674468, latitude: 45.511035447478406}
-// {longitude: -122.6796385, latitude: 45.51103467}
-
   addTreeMarker() {
-    console.log("props check");
-    console.log(this.props);
     this.props.showAddTreeModal();
     this.props.hideAddMarker();
-    // this.props.addTree({ id: "300069", location: { longitude: this.props.addMarker.longitude, latitude: this.props.addMarker.latitude }, type: "Dingleberry", edible: "nut", condition: "sweet", size: "Y"  });
-
   }
 
   render() {
-
     const {height, width} = Dimensions.get('window');
-    const data = (!this.props.markers.length) ? this.state.pins : this.props.markers;
-    const LatLng = this.props.addMarker;
-    console.log(LatLng);
+    const data = this.props.markers;
     return (
       <View style={{height: '100%', width: '100%'}}>
         <MapView
             showsUserLocation={true}
             data={this.props.markers}
-            initialRegion={(this.props.viewCoords) ? this.props.viewCoords : INIT_REGION}
-            onRegionChangeComplete={(region) => {this.props.setViewCoords(region); console.log(this.props);}}
+            initialRegion={this.props.viewCoords}
+            onRegionChangeComplete={(region) => {this.props.setViewCoords(region)}}
             radius={ 48 }>
-            <AddMarker visible={this.props.addMarker.visible} coordinate={{ latitude: this.props.viewCoords.latitude, longitude: this.props.viewCoords.longitude
+            <AddMarker
+              visible={this.props.addMarker.visible}
+              coordinate={{
+                latitude: this.props.viewCoords.latitude,
+                longitude: this.props.viewCoords.longitude
             }}></AddMarker>
         </MapView>
-        <AddTreeModal visible={this.props.addTreeModal.visible} />
+        <AddTreeModal
+          visible={this.props.addTreeModal.visible} />
         <Button
           title={(this.props.addMarker.visible) ? "Create Tree at Marker" : "Add Tree"}
           type={'standard'}
