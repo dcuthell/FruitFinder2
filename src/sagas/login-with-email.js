0fa15cfeea3a27760ce337dde7f0f43527bf59a1
David Cuthell <dcc1447@gmail.com>
2018-04-19 12:29:52 -0700

    fix issue with sign in with email

--- src/sagas/login-with-email.js
+++ src/sagas/login-with-email.js
@@ -9,18 +9,12 @@ import getUserData from '../selectors/userData';
 function* loginWithEmail(action) {
   console.log("login with email saga started");
   try {
-    console.log("Sign up start...");
     const email = action.payload.email;
     const password = action.payload.password;
-    const createresult = yield firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password);
-    console.log("...complete");
-    console.log(createresult);
     console.log("Sign in start...");
     const currentUser = yield firebase.auth().signInAndRetrieveDataWithEmailAndPassword(email, password)
     console.log("...complete");
-    console.log(currentUser);
-    let newUserData = { userInfo : currentUser.user['_user'], authType : 'email' }
-    (newUserData.userInfo.displayName === null) ? "Validated User" : newUserData.userInfo.displayName;
+    let newUserData = { userInfo : currentUser.user['_user'], authType : 'email' } ;
     yield put(setUserData(newUserData));
   } catch (error){
     console.warn(error);
